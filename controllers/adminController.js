5c14f32d428ef24bd67731b3b8d1a72b525338d8
ImEldin <popara.eldin@gmail.com>
2025-12-06 15:33:10 +0100

    Admin dashboard UI, and Login spinner

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,7 +5,9 @@ const VALID_ROLES = ["ucenik", "profesor", "kantina"];
 module.exports = {
     showDashboard(req, res) {
         try {
-            res.render('admin/dashboard');
+            const user = req.session.user;
+            console.log(user);
+            res.render('admin/dashboard', { user });
         } catch (err) {
             console.error(err);
             res.status(500).render('error', { message: 'Failed to load dashboard.' });
